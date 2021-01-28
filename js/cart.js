daf91af90daacc0fc05ebf16c543d77c228893aa
Hasib787 <mr.hasib45@gmail.com>
2021-01-28 19:17:55 +0600

    adding a function to remove cart item

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,22 @@
-console.log('test');
+//remove item event handler
+const removeBtn1=document.getElementById('remove-item-1');
+removeBtn1.addEventListener('click',function(){
+    const cartItem=document.getElementById('cart-1');
+    cartItem.style.display='none';
+});
+
+const removeBtn2=document.getElementById('remove-item-2');
+removeBtn2.addEventListener('click',function(){
+    const cartItem=document.getElementById('cart-2');
+    cartItem.style.display='none';
+});
+
+
+//Plus button event handler
+function increase(clickPlus){
+    const plusBtn=document.getElementById('plus');
+        plusBtn.addEventListener('click', function(){
+        });
+ 
+    
+}
