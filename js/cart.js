d44c3cb6e25a5183137789db9601c7760070a720
Hasib787 <mr.hasib45@gmail.com>
2021-01-28 23:39:04 +0600

    adding a case increase even handler

--- js/cart.js
+++ js/cart.js
@@ -1,30 +1,33 @@
 //remove item event handler
-const removeBtn1=document.getElementById('remove-item-1');
-removeBtn1.addEventListener('click',function(){
-    const cartItem=document.getElementById('cart-1');
-    cartItem.style.display='none';
+const removeBtn1 = document.getElementById('remove-item-1');
+removeBtn1.addEventListener('click', function () {
+    const cartItem = document.getElementById('cart-1');
+    cartItem.style.display = 'none';
 });
 
-const removeBtn2=document.getElementById('remove-item-2');
-removeBtn2.addEventListener('click',function(){
-    const cartItem=document.getElementById('cart-2');
-    cartItem.style.display='none';
+const removeBtn2 = document.getElementById('remove-item-2');
+removeBtn2.addEventListener('click', function () {
+    const cartItem = document.getElementById('cart-2');
+    cartItem.style.display = 'none';
 });
 
 
 //Plus button event handler
-function increase(clickPlus){
-    const plusBtn=document.getElementById('plus');
-        plusBtn.addEventListener('click', function(){
-        });
- 
-    
-}
+const plusBtn = document.getElementById('case-increase');
+plusBtn.addEventListener('click', function () {
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText=caseTotal;
+});
+
 
 //Check Out button event handler
 const checkBtn = document.getElementById('check-out');
 checkBtn.addEventListener('click', function () {
-    const cartArea = document.getElementById("cart-section");
+    const cartArea = document.getElementById("cart-area");
     cartArea.style.display = 'none';
     const checkArea = document.getElementById('thank-you');
     checkArea.style.display = 'block';
